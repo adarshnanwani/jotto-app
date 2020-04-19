b171652fed73f3d93e78b14a3f1cbdf5bb251971
Adarsh Nanwani <adarsh.nanwani@gmail.com>
2020-04-20 00:05:31 +0530

    Add check-prop-types package and utility method.

--- test/testUtils.js
+++ test/testUtils.js
@@ -1,3 +1,5 @@
+import checkPropTypes from 'check-prop-types';
+
 /**
  * Return node(s) with the given test-data attribute.
  * @param {ShallowWrapper} wrapper - Enzyme shallow wrapper.
@@ -6,4 +8,14 @@
  */
 export const findByTestAttr = (wrapper, val) => {
   return wrapper.find(`[data-test='${val}']`);
+};
+
+export const checkProps = (component, confirmingProps) => {
+  const propError = checkPropTypes(
+    component.propTypes,
+    confirmingProps,
+    'prop',
+    component.name
+  );
+  expect(propError).toBeUndefined();
 };
