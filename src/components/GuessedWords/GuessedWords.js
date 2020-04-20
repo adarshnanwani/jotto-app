2074b2fd697efe268e147b4a31af119368c63dd9
Adarsh Nanwani <adarsh.nanwani@gmail.com>
2020-04-20 21:38:43 +0530

    Add code for tests for if there are words guessed.

--- src/components/GuessedWords/GuessedWords.js
+++ src/components/GuessedWords/GuessedWords.js
@@ -2,12 +2,32 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const GuessedWords = ({ guessedWords }) => {
+  const guessedWordsRows = guessedWords.map((word) => (
+    <tr key={word.guessedWord} data-test='guessed-word'>
+      <td>{word.guessedWord}</td>
+      <td>{word.letterMatchCount}</td>
+    </tr>
+  ));
   return (
     <div data-test='component-guessed-words'>
       {guessedWords && guessedWords.length === 0 && (
         <span data-test='guess-instructions'>
           Try to guess the secret word!
         </span>
+      )}
+      {guessedWords && guessedWords.length > 0 && (
+        <div data-test='guessed-words'>
+          <h3>Guessed Words</h3>
+          <table>
+            <thead>
+              <tr>
+                <th>Guess</th>
+                <th>Matching Letters</th>
+              </tr>
+            </thead>
+            <tbody>{guessedWordsRows}</tbody>
+          </table>
+        </div>
       )}
     </div>
   );
