06a805df67b3b1a6a935c0be0f9e116c822cd5ca
Adarsh Nanwani <adarsh.nanwani@gmail.com>
2020-04-22 00:03:37 +0530

    Add tests and code for success=false for input component.

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -2,15 +2,29 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 class Input extends Component {
-  render(){
-    return (
-      <div></div>
-    )
+  render() {
+    const contents = this.props.success ? null : (
+      <form className='form-inline'>
+        <input
+          type='text'
+          data-test='input-box'
+          className='mb-2 mx-sm-3'
+          placeholder='Enter guess'
+        />
+        <input
+          type='submit'
+          value='Submit'
+          data-test='submit-button'
+          className='btn btn-primary'
+        />
+      </form>
+    );
+    return <div data-test='component-input'>{contents}</div>;
   }
 }
 
-const mapStateToProps = state => ({
-
-})
+const mapStateToProps = ({ success }) => ({
+  success,
+});
 
 export default connect(mapStateToProps)(Input);
