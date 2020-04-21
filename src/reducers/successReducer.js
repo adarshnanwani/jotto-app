8840b68fdbef95afc92fe1b9eb39c12da8288ea2
Adarsh Nanwani <adarsh.nanwani@gmail.com>
2020-04-21 14:11:11 +0530

    Add code for reducer with passing tests.

--- src/reducers/successReducer.js
+++ src/reducers/successReducer.js
@@ -1,9 +1,17 @@
+import { actionTypes } from '../actions/index';
+const initialState = false;
+
 /**
  * @function successReducer
  * @param {array} state - Array of guessed words.
  * @param {object} action - action to be reduced.
  * @returns {boolean} - new success state.
  */
-export default (state, action) => {
-  return null;
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case actionTypes.CORRECT_GUESS:
+      return true;
+    default:
+      return state;
+  }
 };
