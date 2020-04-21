0d73a3aa1f7d6acb4b112e2f10601ff51173a175
Adarsh Nanwani <adarsh.nanwani@gmail.com>
2020-04-21 15:22:51 +0530

    Add tests for Input component.

---
+++ src/components/Input/Input.test.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { findByTestAttr } from '../../../test/testUtils';
+
+describe('render', () => {
+  describe('word has not been guessed', () => {
+    test('renders component without error', () => {});
+    test('renders input box', () => {});
+    test('renders submit button', () => {});
+  });
+  describe('word has been guessed', () => {
+    test('renders component without error', () => {});
+    test('does not renders input box', () => {});
+    test('does not renders submit button', () => {});
+  });
+});
+
+describe('update state', () => {});
