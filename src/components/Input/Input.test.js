a0e896a03eca3896d8b313b6d2529338de40c9eb
Adarsh Nanwani <adarsh.nanwani@gmail.com>
2020-04-21 16:00:57 +0530

    Add setup method for Input tests.

--- src/components/Input/Input.test.js
+++ src/components/Input/Input.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, dive } from 'enzyme';
 import { findByTestAttr, storeFactory } from '../../../test/testUtils';
 import Input from './Input';
 
@@ -11,11 +11,11 @@ import Input from './Input';
  */
 const setup = (initialState = {}) => {
   const store = storeFactory(initialState);
-  const wrapper = shallow(<Input store={store} />);
-  console.log(wrapper.debug());
+  const wrapper = shallow(<Input store={store} />)
+    .dive()
+    .dive();
+  return wrapper;
 };
-
-setup();
 
 describe('render', () => {
   describe('word has not been guessed', () => {
