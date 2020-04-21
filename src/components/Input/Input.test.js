24de44275d4402bc26ac6b0c6cd90b7e3e66cf37
Adarsh Nanwani <adarsh.nanwani@gmail.com>
2020-04-21 15:53:15 +0530

    Add storeFactory to test utils.

--- src/components/Input/Input.test.js
+++ src/components/Input/Input.test.js
@@ -1,6 +1,21 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import { findByTestAttr } from '../../../test/testUtils';
+import { findByTestAttr, storeFactory } from '../../../test/testUtils';
+import Input from './Input';
+
+/**
+ * Factory function to create a ShallowWrapper for the Input component.
+ * @function setup
+ * @param {object} initialState - Initial state of this setup.
+ * @param {ShallowWrapper}
+ */
+const setup = (initialState = {}) => {
+  const store = storeFactory(initialState);
+  const wrapper = shallow(<Input store={store} />);
+  console.log(wrapper.debug());
+};
+
+setup();
 
 describe('render', () => {
   describe('word has not been guessed', () => {
--- test/testUtils.js
+++ test/testUtils.js
@@ -1,4 +1,17 @@
 import checkPropTypes from 'check-prop-types';
+import { createStore } from 'redux';
+import rootReducer from '../src/reducers/index';
+
+/**
+ * Create a testing store with imported reducers, middleware, and initial state.
+ * @function storeFactory
+ * @global: rootReducer
+ * @param {object} initialState - Initial state for store.
+ * @returns {Store} - Redux store
+ */
+export const storeFactory = (initialState) => {
+  return createStore(rootReducer, initialState);
+};
 
 /**
  * Return node(s) with the given test-data attribute.
